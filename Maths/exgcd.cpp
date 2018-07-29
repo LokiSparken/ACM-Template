cc7bf67fce4adc1817e63c62c3d6b3a04030ec73
sparken <sparken@qq.com>
2018-07-29 18:21:22 +0800

    update

--- Maths/exgcd.cpp
+++ Maths/exgcd.cpp
@@ -2,10 +2,7 @@
 //对于方程ax+by=c;要求c能被gcd(a,b)整除
 void exgcd(int a,int b,int &d,int &x,int &y)
 {
-    if(!b)
-    {
-        x=1;y=0;d=a;
-    }
+    if(!b) x=1;y=0;d=a;
     else
     {
         exgcd(b,a%b,d,y,x);
