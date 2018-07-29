8ab6256431bc7e97a3e4fa14b4273ec6744b7797
sparken <sparken@qq.com>
2018-07-29 23:14:48 +0800

    update

--- Maths/Prime/EularSieve.cpp
+++ Maths/Prime/EularSieve.cpp
@@ -1,5 +1,21 @@
+/* 求一个数的phi，时间复杂度sqrt(n) */
+inline ll Phi(ll num)
+{
+    int ans = num;
+    for (int i = 2; i * i <= num; ++i)
+        if (num % i == 0)
+        {
+            ans -= ans / i;
+            while (num % i == 0)
+                num /= i;
+        }
+    if (num > 1)
+        ans -= ans / num;
+    return phi[num] = ans;
+}
+
 /* 
- * O(n)得到欧拉函数phi[]、素数表prime[]、素数个数tot
+ * 线性筛O(n)得1e7内所有数的欧拉函数phi[]、素数表prime[]、素数个数tot
  * 不要phi时可以把所有关于phi的表达式去掉（别去break），传入的n为函数定义域上界。
  */
 bool vis[maxn];
@@ -15,14 +31,16 @@ void CalPhi(int n)
             prime[tot++] = i, phi[i] = i - 1;
         for (int j = 0; j < tot; j++)
         {
-            if (i * prime[j] > n) break;
+            if (i * prime[j] > n)
+                break;
             vis[i * prime[j]] = 1;
             if (i % prime[j] == 0)
             {
                 phi[i * prime[j]] = phi[i] * prime[j];
                 break;
             }
-            else phi[i * prime[j]] = phi[i] * (prime[j] - 1);
+            else
+                phi[i * prime[j]] = phi[i] * (prime[j] - 1);
         }
     }
 }
